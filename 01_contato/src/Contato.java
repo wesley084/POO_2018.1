3b964aac886eacb0790481d7cbcb8bbf867f5ccd
Wesley Oliveira Castro <31354309+wesley084@users.noreply.github.com>
2018-06-28 00:31:09 -0300

    Update Contato.java

--- 01_contato/src/Contato.java
+++ 01_contato/src/Contato.java
@@ -44,6 +44,20 @@ public class Contato {
 			}
 		return("fail: fone " + _identifi + " nao existe");
 	}
+	
+	String update(String cont, String[] contatos) {
+		if(!cont.equals(this.nome))
+			throw new RuntimeException("Contato nao existe!");
+		for (int i = 2; i < contatos.length; i++) {
+			String [] tupla = contatos[i].split(":");
+			for (Numero num : this.numeros)
+				if(num.nIdentificador.equals(tupla[0]))
+					break;
+			this.numeros.add(new Numero(tupla[0], tupla[1]));
+		}
+		return "done";
+	}
+	
 }
 
 class Interface{
@@ -52,20 +66,21 @@ class Interface{
 		
 		while(true){
 			String line = Poo.input("Digite um comando: ");
-			ArrayList<String> ui = Poo.split(line, " ");
+			String[] ui = line.split(" ");
 			try {
-				switch (ui.get(0)) {
+				switch (ui[0]) {
 				case "help":{
 					Poo.print("init $nome :: cria o contato\n"
 							+ "show :: mostra o contato\n"
 							+ "addFone $identificador $numero :: adiciona um numero\n"
 							+ "rmFone $identificador :: remove o numero correspondente\n"
-							+ "exit :: finaliza o programa\n");
+							+ "exit :: finaliza o programa\n"
+							+ "update :: atualiza contatos \n");
 					break;
 				}
 				case "show":{
 					if(contact.nome.equals(" "))
-						Poo.print("fail: contato não iniciado\n");
+						Poo.print("fail: contato nao iniciado\n");
 					else {
 						System.out.print(contact.nome + " ");
 						for(Numero n : contact.numeros) {
@@ -76,17 +91,21 @@ class Interface{
 					break;
 				}
 				case "init":{
-					contact.nome = ui.get(1);
+					contact.nome = ui[1];
 					contact.numeros.clear();
 					Poo.print("done");
 					break;
 				}		
 				case "addFone":{
-					Poo.print(contact.addFone(ui.get(1), ui.get(2)));
+					Poo.print(contact.addFone(ui[1], ui[2]));
 					break;
 				}
 				case "rmFone":{
-					Poo.print(contact.rmFone(ui.get(1)));
+					Poo.print(contact.rmFone(ui[1]));
+					break;
+				}
+				case "update":{
+					Poo.print(contact.update(ui[1], ui));
 					break;
 				}
 				case "exit":{
